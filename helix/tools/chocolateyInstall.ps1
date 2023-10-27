a5c0374da5b3babb6273a34a768de0351e80679f
Chocolatey <chocolatey@realdimensions.net>
2023-10-27 00:26:16 +0000

    AU: 1 updated - helix [skip ci]

--- helix/tools/chocolateyInstall.ps1
+++ helix/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'helix'
-$url = 'https://github.com/helix-editor/helix/releases/download/23.05/helix-23.05-x86_64-windows.zip'
+$url = 'https://github.com/helix-editor/helix/releases/download/23.10/helix-23.10-x86_64-windows.zip'
 $directoryName = (Split-Path $url -Leaf) -replace ".zip"
 
 $packageArgs = @{
   packageName    = $packageName
   url64bit       = $url
-  checksum64     = '27ba9a7ea0cbb5a4de2378c5d7383405f2aeb254118cfba1785eb0b32aee3ef0'
+  checksum64     = '16c5201ff48bbd3ad0bc8fa4d75f2f30307f3e1d002a40bbea0fcfe1f55d0d28'
   checksumType64 = 'sha256'
   unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
 }
