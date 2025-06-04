01af781866aaa8278019c74a6ce3c4873483935c
Mikhail Katychev <mkatych@gmail.com>
2025-06-04 12:05:49 -0500

    updated helix to latest version

--- helix/tools/chocolateyInstall.ps1
+++ helix/tools/chocolateyInstall.ps1
@@ -1,13 +1,13 @@
 ﻿$ErrorActionPreference = 'Stop'
 
 $packageName = 'helix'
-$url = 'https://github.com/helix-editor/helix/releases/download/24.07/helix-24.07-x86_64-windows.zip'
+$url = 'https://github.com/helix-editor/helix/releases/download/25.01.1/helix-25.01.1-x86_64-windows.zip'
 $directoryName = (Split-Path $url -Leaf) -replace ".zip"
 
 $packageArgs = @{
   packageName    = $packageName
   url64bit       = $url
-  checksum64     = '3575f28a3e718005ffd803fa04ae8d26c83ba72131615182beafe7152ce831dd'
+  checksum64     = '564055657cf24a4ebb0ef929ee2bc40d379431868e06fd82e41a0def01a5a320'
   checksumType64 = 'sha256'
   unzipLocation  = Split-Path $MyInvocation.MyCommand.Definition
 }
